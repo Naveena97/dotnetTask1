972c5cdc831ebb804454ee1df6d5911ce80bb893
Naveena97 <naveenaannavaram@gmail.com>
2023-10-30 10:38:41 +0530

    first commit

---
+++ dotnetCoreTask1/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace dotnetCoreTask1.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
